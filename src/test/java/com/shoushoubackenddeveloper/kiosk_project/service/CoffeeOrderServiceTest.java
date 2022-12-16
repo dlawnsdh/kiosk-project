ab6ceffccf1dff8f50450721678338720f33d90f
bonojuno <gtx540@naver.com>
2022-12-17 00:16:07 +0900

    서비스 테스트 작성
    서비스 테스트 클래스만 생성해 놓은 상태
    추후 테스트 코드 작성 예정

---
+++ src/test/java/com/shoushoubackenddeveloper/kiosk_project/service/CoffeeOrderServiceTest.java
@@ -0,0 +1,7 @@
+package com.shoushoubackenddeveloper.kiosk_project.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoffeeOrderServiceTest {
+
+}
---
+++ src/test/java/com/shoushoubackenddeveloper/kiosk_project/service/CoffeeServiceTest.java
@@ -0,0 +1,7 @@
+package com.shoushoubackenddeveloper.kiosk_project.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CoffeeServiceTest {
+
+}
