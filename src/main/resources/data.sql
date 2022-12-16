5c6d8aa7d70af725acb9a8ece33afdcc731a4376
bonojuno <gtx540@naver.com>
2022-12-17 00:08:39 +0900

    목업 데이터 작성
    data.sql을 생성하여 기본적으로 정보가 존재해야 하는
    커피 테이블과 옵션 테이블에 컬럼 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,11 @@
-insert into option (created_at,modified_at,eng_name,kor_name,price) values (now(),now(),'syrup','시럽',300);
+insert into option (created_at,modified_at,eng_name,kor_name,price) values
+    (now(),now(),'syrup','시럽',300),
+    (now(),now(),'shot','샷',500);
+
+insert into coffee (created_at, modified_at, coffee_code, eng_name, kor_name, order_status, price, size_selectable) values
+    (now(), now(), 'h001', 'americano', '아메리카노', '판매중', 4500, true),
+    (now(), now(), 'c001', 'ice_americano', '아이스_아메리카노', '판매중', 4500, true),
+    (now(), now(), 'h002', 'caffe_latte', '카페라떼', '판매중', 5000, true),
+    (now(), now(), 'c002', 'ice_caffe_latte', '아이스_카페라떼', '판매중', 5000, true),
+    (now(), now(), 'h003', 'caffe_mocha', '카페모카', '판매중', 5500, true),
+    (now(), now(), 'c003', 'ice_caffe_mocha', '아이스_카페모카', '판매중', 5500, true);
